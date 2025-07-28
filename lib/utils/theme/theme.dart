849368146fdfeaf3725469af2cb113e9724f31c4
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-28 18:02:20 +0600

    theme build

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:e_dokan/utils/constants/colors.dart';
+import 'package:e_dokan/utils/theme/widgets/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class UAppTheme {
@@ -10,7 +11,14 @@ class UAppTheme {
     brightness: Brightness.light,
     primaryColor: UColors.primary,
     disabledColor: UColors.grey,
-    textTheme: TextTheme(),
+    textTheme: UTextTheme.lightTheme,
   );
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: '',
+    brightness: Brightness.light,
+    primaryColor: UColors.primary,
+    disabledColor: UColors.grey,
+    textTheme: UTextTheme.darkTheme,
+  );
 }
