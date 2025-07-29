c0899787fa62078ccb534312cb2a0bcfbdbfab4a
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-29 15:49:34 +0600

    folder name

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,12 +1,12 @@
 import 'package:e_dokan/utils/constants/colors.dart';
-import 'package:e_dokan/utils/theme/widgets/appbar_theme.dart';
-import 'package:e_dokan/utils/theme/widgets/bottom_sheet_theme.dart';
-import 'package:e_dokan/utils/theme/widgets/checkbox_theme.dart';
-import 'package:e_dokan/utils/theme/widgets/chip_theme.dart';
-import 'package:e_dokan/utils/theme/widgets/elevated_button_theme.dart';
-import 'package:e_dokan/utils/theme/widgets/outlined_button_theme.dart';
-import 'package:e_dokan/utils/theme/widgets/text_field_theme.dart';
-import 'package:e_dokan/utils/theme/widgets/text_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/appbar_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/bottom_sheet_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/checkbox_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/chip_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/elevated_button_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/outlined_button_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/text_field_theme.dart';
+import 'package:e_dokan/utils/theme/widgets_theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class UAppTheme {
