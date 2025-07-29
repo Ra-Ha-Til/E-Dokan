a66214825d94d24819120d5f928b0601cbd6e303
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-29 12:36:15 +0600

    appbar theme

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.system,
       theme: UAppTheme.lightTheme,
       darkTheme: UAppTheme.darkTheme,
       home: Scaffold(),
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,11 @@
 import 'package:e_dokan/utils/constants/colors.dart';
+import 'package:e_dokan/utils/theme/widgets/appbar_theme.dart';
+import 'package:e_dokan/utils/theme/widgets/bottom_sheet_theme.dart';
+import 'package:e_dokan/utils/theme/widgets/checkbox_theme.dart';
+import 'package:e_dokan/utils/theme/widgets/chip_theme.dart';
+import 'package:e_dokan/utils/theme/widgets/elevated_button_theme.dart';
+import 'package:e_dokan/utils/theme/widgets/outlined_button_theme.dart';
+import 'package:e_dokan/utils/theme/widgets/text_field_theme.dart';
 import 'package:e_dokan/utils/theme/widgets/text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +18,15 @@ class UAppTheme {
     brightness: Brightness.light,
     primaryColor: UColors.primary,
     disabledColor: UColors.grey,
-    textTheme: TextTheme(),
+    textTheme: UTextTheme.lightTextTheme,
+    chipTheme: UChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: UColors.white,
+    appBarTheme: UAppBarTheme.lightAppBarTheme,
+    checkboxTheme: UCheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: UBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: UElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: UOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: UTextFormFieldTheme.lightInputDecorationTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -19,6 +34,14 @@ class UAppTheme {
     brightness: Brightness.dark,
     primaryColor: UColors.primary,
     disabledColor: UColors.grey,
-    textTheme: TextTheme(),
+    textTheme: UTextTheme.darkTextTheme,
+    chipTheme: UChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: UColors.white,
+    appBarTheme: UAppBarTheme.darkAppBarTheme,
+    checkboxTheme: UCheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: UBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: UElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: UOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: UTextFormFieldTheme.darkInputDecorationTheme,
   );
 }
